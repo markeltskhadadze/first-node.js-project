aea1519a4e863e4639451e7184603238046648eb
markeltskhadadze <markeltskhadadze@gmail.com>
2023-04-30 10:17:06 +0400

    fix get route

--- models/reviews.js
+++ models/reviews.js
@@ -4,12 +4,10 @@ const reviews = new Schema({
   fullName: {
     type: String,
     required: true,
-    allowNull: false
   },
   reviewText: {
     type: String,
     required: true,
-    allowNull: false
   }
 })
 
